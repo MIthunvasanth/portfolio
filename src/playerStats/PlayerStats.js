fd91eb98b23081782f8e513a3706790469ae14c3
JERRY <101572307+MIthunvasanth@users.noreply.github.com>
2024-11-23 19:58:52 +0530

    ui changse;

--- src/playerStats/PlayerStats.js
+++ src/playerStats/PlayerStats.js
@@ -1,21 +1,29 @@
 import React from "react";
 import "../styles/playerStats.css";
-import Linkedicon from "../assets/linkedin.png"
-import Instaicon from "../assets/instagram.png"
-import Giticon from "../assets/github.png"
+import Linkedicon from "../assets/linkedin.png";
+import Instaicon from "../assets/instagram.png";
+import Giticon from "../assets/github.png";
+
 const PlayerStats = () => {
   return (
     <div id="playerStats">
-      <h1>MTHUN VASANTH</h1>
+      {/* Player's Name */}
+      <h1>MITHUN VASANTH</h1>
+
+      {/* Lines below the name */}
       <div id="playerStats__lines">
         <span id="playerStats__lines__thick"></span>
         <span id="playerStats__lines__thin"></span>
       </div>
+
+      {/* Player's Title */}
       <h2>Fullstack Developer / AI Developer</h2>
-      <div className="social">    
-        <img src={Linkedicon}/>
-        <img src={Instaicon}/>
-        <img src={Giticon}/>
+
+      {/* Social Media Icons */}
+      <div className="social">
+        <img src={Linkedicon} alt="LinkedIn Icon" className="social-icon" />
+        <img src={Instaicon} alt="Instagram Icon" className="social-icon" />
+        <img src={Giticon} alt="GitHub Icon" className="social-icon" />
       </div>
     </div>
   );
